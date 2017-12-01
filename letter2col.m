dba903e0d3b8c92523e24b9588a55c542b8b2ba7
Jacob Killelea <Jkillelea@protonmail.ch>
2017-12-01 11:52:16 -0700

    correct numbers for arguments past AZ

--- letter2col.m
+++ letter2col.m
@@ -1,6 +1,9 @@
 function col = letter2col(str)
 % Change CSV spreadsheet columns 'A', 'B', 'C' ... 'AA', 'ZZ' ...
-% to a zero-based index           0,   1,   2  ...  26,   51 ...
-  col = 26*(length(str) -1); % AA is position 26
+% to a zero-based index           0,   1,   2  ...  26,   675 ...
+  col = 0;
+  for i = 1:length(str)-1
+    col = col + 26*(str(i) - 64); % AA is position 26
+  end
   col = col + str(end) - 65;
 end
