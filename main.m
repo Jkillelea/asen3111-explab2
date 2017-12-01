a7c916b511041a50907c1be8d4a3b3bd8c58e0f6
Jacob Killelea <Jkillelea@protonmail.ch>
2017-12-01 11:51:44 -0700

    misc

--- main.m
+++ main.m
@@ -3,7 +3,7 @@ clc;
 close all;
 
 data_dir = 'data/';
-% alphas = -11:20;
+alphas = -11:20;
 
 [short, long] = getfiles(data_dir);
 data = [short; long];
@@ -22,7 +22,7 @@ for i = 1:length(data)
   % dynamic pressure (Pa)
   q15 = mean(0.5 * rho(speed15) .* airspeed(speed15).^2);
   q25 = mean(0.5 * rho(speed25) .* airspeed(speed25).^2);
-
+  
   % remove zero-airspeed forces
   N15 = N(speed15) - N(speed0);
   A15 = A(speed15) - A(speed0);
@@ -30,41 +30,36 @@ for i = 1:length(data)
   N25 = N(speed25) - N(speed0);
   A25 = A(speed25) - A(speed0);
   M25 = M(speed25) - M(speed0);
-
+ 
   % lift and drag from normal and axial force (Newtons)
   L15 = N15.*cosd(alpha(speed15)) - A15.*sind(alpha(speed15));
   D15 = N15.*sind(alpha(speed15)) + A15.*cosd(alpha(speed15));
   L25 = N25.*cosd(alpha(speed25)) - A25.*sind(alpha(speed25));
   D25 = N25.*sind(alpha(speed25)) + A25.*cosd(alpha(speed25));
-
+  
   figure; hold on; grid on; % 25 m/s plot
   l = fit(alpha(speed25), L25, 'smoothingspline');
   d = fit(alpha(speed25), D25, 'smoothingspline');
   plot(alpha(speed25), feval(l, alpha(speed25)), 'displayname', 'Lift');
   plot(alpha(speed25), feval(d, alpha(speed25)), 'displayname', 'Drag');
-  title(sprintf('%s wing, %s angles, 25 m/s', file_info.type, file_info.angles));
+  title(sprintf('%s wing, %s angles, 25 m/s', ...
+                file_info.type, file_info.angles));
   xlabel('Angle of Attack (degrees)');
   ylabel('Force (N)');
   legend('show', 'location', 'southeast');
-
-  filename = sprintf('section%d_group%d_%s_%s_25ms', ...
-      file_info.section, file_info.group, file_info.angles, file_info.type);
-  print(['graphs/', filename], '-dpng');
-
-
+  print(['graphs/', gen_filename(file_info, 25)], '-dpng');
+  
   figure; hold on; grid on; % 15 m/s plot
   l = fit(alpha(speed15), L15, 'smoothingspline');
   d = fit(alpha(speed15), D15, 'smoothingspline');
   plot(alpha(speed25), feval(l, alpha(speed25)), 'displayname', 'Lift');
   plot(alpha(speed25), feval(d, alpha(speed25)), 'displayname', 'Drag');
-  title(sprintf('%s wing, %s angles, 15 m/s', file_info.type, file_info.angles));
+  title(sprintf('%s wing, %s angles, 15 m/s', ...
+                file_info.type, file_info.angles));
   xlabel('Angle of Attack (degrees)');
   ylabel('Force (N)');
   legend('show', 'location', 'southeast');
-
-  filename = sprintf('section%d_group%d_%s_%s_15ms', ...
-      file_info.section, file_info.group, file_info.angles, file_info.type);
-  print(['graphs/', filename], '-dpng');
-
+  
+  print(['graphs/', gen_filename(file_info, 15)], '-dpng');
   close all;
 end
