7f85043c9d00c6578af9f9f70abf5b4d3649adf9
Jacob Killelea <Jkillelea@protonmail.ch>
2017-12-10 21:06:18 -0700

    hangle elliptical wings

--- getfiles.m
+++ getfiles.m
@@ -1,4 +1,5 @@
-function [short, long] = getfiles(data_dir)
-  short = dir([data_dir '*Short.csv']);
-  long  = dir([data_dir '*Long.csv']);
+function [short, long, elliptical] = getfiles(data_dir)
+  short       = dir([data_dir '*Short.csv']);
+  long        = dir([data_dir '*Long.csv']);
+  elliptical  = dir([data_dir '*Elliptical.csv']);
 end
